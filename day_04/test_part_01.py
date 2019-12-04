60ef03d7ab496da593b3833e71764087de515de8
jamsidedown <robertanderson1993@googlemail.com>
2019-12-04 14:38:01 +0000

    Forgot to update unit tests for day 4 part 1

--- day_04/test_part_01.py
+++ day_04/test_part_01.py
@@ -1,17 +1,14 @@
 import unittest
-from part_01 import passes_criteria
+from part_01 import match
 
 
 class Part01Tests(unittest.TestCase):
 
     def test_111111_passes(self):
-        passes = passes_criteria(111111)
-        self.assertTrue(passes)
+        self.assertTrue(match(111111))
 
     def test_223450_fails(self):
-        passes = passes_criteria(223450)
-        self.assertFalse(passes)
+        self.assertFalse(match(223450))
 
     def test_123789_fails(self):
-        passes = passes_criteria(123789)
-        self.assertFalse(passes)
+        self.assertFalse(match(123789))
