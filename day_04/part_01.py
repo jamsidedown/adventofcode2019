686a9a16dc28bcbd23cbe2fce61e7c2198c6b14d
jamsidedown <robertanderson1993@googlemail.com>
2019-12-04 14:36:51 +0000

    Refactored part 1 down to a few lines

--- day_04/part_01.py
+++ day_04/part_01.py
@@ -1,17 +1,8 @@
-def passes_criteria(n: int) -> bool:
-    ns = str(n)
-
-    for i in range(len(ns) - 1):
-        if ns[i] > ns[i + 1]:
-            return False
-
-    return len(set(ns)) < len(ns)
+def match(ns: str) -> bool:
+    return sorted(ns) == [*ns] and len({*ns}) < len(ns)
 
 
 if __name__ == '__main__':
-    min = 357253
-    max = 892942
-
-    pass_count = sum(1 for i in range(min, max + 1) if passes_criteria(i))
-    
-    print(f'different passwords: {pass_count}')
+    min, max = 357253, 892942
+    matches = sum(1 for n in range(min, max + 1) if match(str(n)))
+    print(f'different passwords: {matches}')
