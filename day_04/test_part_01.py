3ff184209452d9a19db19ee3364a037aa2a76c3e
jamsidedown <robertanderson1993@googlemail.com>
2019-12-04 14:40:12 +0000

    Forgot to stringify ints in test for d4p1

--- day_04/test_part_01.py
+++ day_04/test_part_01.py
@@ -5,10 +5,10 @@ from part_01 import match
 class Part01Tests(unittest.TestCase):
 
     def test_111111_passes(self):
-        self.assertTrue(match(111111))
+        self.assertTrue(match(str(111111)))
 
     def test_223450_fails(self):
-        self.assertFalse(match(223450))
+        self.assertFalse(match(str(223450)))
 
     def test_123789_fails(self):
-        self.assertFalse(match(123789))
+        self.assertFalse(match(str(123789)))
