391c842f994287c34501a67f10aecb161962e803
jamsidedown <robertanderson1993@googlemail.com>
2019-12-05 12:23:37 +0000

    Refactored d5p2

--- day_05/part_02.py
+++ day_05/part_02.py
@@ -1,59 +1,48 @@
 from typing import List
 
 
-def run(p: List[str], input_parameter: str) -> str:
+def run(p: List[str], input_parameter: int):
+    for i in range(len(p)):
+        p[i] = int(p[i])
+
     i = 0
-    while (p[i] != '99'):
-        instruction = p[i].rjust(5, '0')
+    ii = {1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4}
+    
+    while (p[i] != 99):
+        instruction = str(p[i]).rjust(5, '0')
         opcode = int(instruction[-2:])
         modes = [instruction[2], instruction[1], instruction[0]]
         modes = [bool(int(mode)) for mode in modes]
 
-        # add and multiply
-        if opcode == 1 or opcode == 2:
-            first = p[i + 1] if modes[0] else p[int(p[i + 1])]
-            second = p[i + 2] if modes[1] else p[int(p[i + 2])]
-            first, second = int(first), int(second)
-            if opcode == 1:
-                p[int(p[i + 3])] = str(first + second)
-            else:
-                p[int(p[i + 3])] = str(first * second)
-            i += 4
-        # input
+        if opcode in {1, 2, 4, 5, 6, 7, 8}:
+            first = p[i + 1] if modes[0] else p[p[i + 1]]
+        if opcode in {1, 2, 5, 6, 7, 8}:
+            second = p[i + 2] if modes[1] else p[p[i + 2]]
+        if opcode in {1, 2, 7, 8}:
+            address = i + 3 if modes[2] else p[i + 3]
+        
+        if opcode == 1:
+            p[address] = first + second
+        elif opcode == 2:
+            p[address] = first * second
         elif opcode == 3:
-            p[int(p[i + 1])] = input_parameter
-            i += 2
-        # output
+            address = i + 1 if modes[0] else p[i + 1]
+            p[address] = input_parameter
         elif opcode == 4:
-            value = p[i + 1] if modes[0] else p[int(p[i + 1])]
-            print(value)
-            i += 2
-        # jump if true or false
-        elif opcode == 5 or opcode == 6:
-            value = int(p[i + 1]) if modes[0] else int(p[int(p[i + 1])])
-            jump = value != 0 if opcode == 5 else value == 0
-            if jump:
-                i = int(p[i + 2]) if modes[1] else int(p[int(p[i + 2])])
-            else:
-                i += 3
-        # less than and equals
-        elif opcode == 7 or opcode == 8:
-            first = p[i + 1] if modes[0] else p[int(p[i + 1])]
-            second = p[i + 2] if modes[1] else p[int(p[i + 2])]
-            first, second = int(first), int(second)
-            if opcode == 7:
-                value = '1' if first < second else '0'
-            else:
-                value = '1' if first == second else '0'
-            if modes[2]:
-                p[i + 3] = value
-            else:
-                p[int(p[i + 3])] = value
-            i += 4
+            print(first)
+        elif opcode == 5:
+            i = second - ii[opcode] if first != 0 else i
+        elif opcode == 6:
+            i = second - ii[opcode] if first == 0 else i
+        elif opcode == 7:
+            p[address] = int(first < second)
+        elif opcode == 8:
+            p[address] = int(first == second)
+
+        i += ii[opcode]
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         program = f.read().split(',')
-
-    run(program, '5')
+    run(program, 5)
--- day_05/test_part_02.py
+++ day_05/test_part_02.py
@@ -8,95 +8,95 @@ class Part02Tests(unittest.TestCase):
     @patch('builtins.print')
     def test_first_example_true(self, mock_print):
         program = '3,9,8,9,10,9,4,9,99,-1,8'.split(',')
-        run(program, '8')
-        mock_print.assert_called_once_with('1')
+        run(program, 8)
+        mock_print.assert_called_once_with(1)
 
     @patch('builtins.print')
     def test_first_example_false(self, mock_print):
         program = '3,9,8,9,10,9,4,9,99,-1,8'.split(',')
-        run(program, '7')
-        mock_print.assert_called_once_with('0')
+        run(program, 7)
+        mock_print.assert_called_once_with(0)
 
     @patch('builtins.print')
     def test_second_example_true(self, mock_print):
         program = '3,9,7,9,10,9,4,9,99,-1,8'.split(',')
-        run(program, '7')
-        mock_print.assert_called_once_with('1')
+        run(program, 7)
+        mock_print.assert_called_once_with(1)
 
     @patch('builtins.print')
     def test_second_example_false(self, mock_print):
         program = '3,9,7,9,10,9,4,9,99,-1,8'.split(',')
-        run(program, '9')
-        mock_print.assert_called_once_with('0')
+        run(program, 9)
+        mock_print.assert_called_once_with(0)
 
     @patch('builtins.print')
     def test_third_example_true(self, mock_print):
         program = '3,3,1108,-1,8,3,4,3,99'.split(',')
-        run(program, '8')
-        mock_print.assert_called_once_with('1')
+        run(program, 8)
+        mock_print.assert_called_once_with(1)
 
     @patch('builtins.print')
     def test_third_example_false(self, mock_print):
         program = '3,3,1108,-1,8,3,4,3,99'.split(',')
-        run(program, '7')
-        mock_print.assert_called_once_with('0')
+        run(program, 7)
+        mock_print.assert_called_once_with(0)
 
     @patch('builtins.print')
     def test_fourth_example_true(self, mock_print):
         program = '3,3,1107,-1,8,3,4,3,99'.split(',')
-        run(program, '7')
-        mock_print.assert_called_once_with('1')
+        run(program, 7)
+        mock_print.assert_called_once_with(1)
 
     @patch('builtins.print')
     def test_fourth_example_false(self, mock_print):
         program = '3,3,1107,-1,8,3,4,3,99'.split(',')
-        run(program, '9')
-        mock_print.assert_called_once_with('0')
+        run(program, 9)
+        mock_print.assert_called_once_with(0)
 
     @patch('builtins.print')
     def test_fifth_example_true(self, mock_print):
         program = '3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9'.split(',')
-        run(program, '2')
-        mock_print.assert_called_once_with('1')
+        run(program, 2)
+        mock_print.assert_called_once_with(1)
 
     @patch('builtins.print')
     def test_fifth_example_false(self, mock_print):
         program = '3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9'.split(',')
-        run(program, '0')
-        mock_print.assert_called_once_with('0')
+        run(program, 0)
+        mock_print.assert_called_once_with(0)
 
     @patch('builtins.print')
     def test_sixth_example_true(self, mock_print):
         program = '3,3,1105,-1,9,1101,0,0,12,4,12,99,1'.split(',')
-        run(program, '2')
-        mock_print.assert_called_once_with('1')
+        run(program, 2)
+        mock_print.assert_called_once_with(1)
 
     @patch('builtins.print')
     def test_sixth_example_false(self, mock_print):
         program = '3,3,1105,-1,9,1101,0,0,12,4,12,99,1'.split(',')
-        run(program, '0')
-        mock_print.assert_called_once_with('0')
+        run(program, 0)
+        mock_print.assert_called_once_with(0)
 
     @patch('builtins.print')
     def test_seventh_example_below(self, mock_print):
         program = ('3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,' +
                    '1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,' +
                    '999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99').split(',')
-        run(program, '7')
-        mock_print.assert_called_once_with('999')
+        run(program, 7)
+        mock_print.assert_called_once_with(999)
 
     @patch('builtins.print')
     def test_seventh_example_equal(self, mock_print):
         program = ('3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,' +
                    '1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,' +
                    '999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99').split(',')
-        run(program, '8')
-        mock_print.assert_called_once_with('1000')
+        run(program, 8)
+        mock_print.assert_called_once_with(1000)
 
     @patch('builtins.print')
     def test_seventh_example_above(self, mock_print):
         program = ('3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,' +
                   '1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,' +
                   '999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99').split(',')
-        run(program, '9')
-        mock_print.assert_called_once_with('1001')
+        run(program, 9)
+        mock_print.assert_called_once_with(1001)
