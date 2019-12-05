46f0c563a7cd58bac2ab60e65ad07a940689e1ca
jamsidedown <robertanderson1993@googlemail.com>
2019-12-05 11:59:44 +0000

    Added solutino for d5p2

--- day_05/part_02.py
+++ day_05/part_02.py
@@ -1,8 +1,7 @@
 from typing import List
 
 
-def run(p: List[str]) -> str:
-    input_parameter = '1'
+def run(p: List[str], input_parameter: str) -> str:
     i = 0
     while (p[i] != '99'):
         instruction = p[i].rjust(5, '0')
@@ -29,10 +28,32 @@ def run(p: List[str]) -> str:
             value = p[i + 1] if modes[0] else p[int(p[i + 1])]
             print(value)
             i += 2
+        # jump if true or false
+        elif opcode == 5 or opcode == 6:
+            value = int(p[i + 1]) if modes[0] else int(p[int(p[i + 1])])
+            jump = value != 0 if opcode == 5 else value == 0
+            if jump:
+                i = int(p[i + 2]) if modes[1] else int(p[int(p[i + 2])])
+            else:
+                i += 3
+        # less than and equals
+        elif opcode == 7 or opcode == 8:
+            first = p[i + 1] if modes[0] else p[int(p[i + 1])]
+            second = p[i + 2] if modes[1] else p[int(p[i + 2])]
+            first, second = int(first), int(second)
+            if opcode == 7:
+                value = '1' if first < second else '0'
+            else:
+                value = '1' if first == second else '0'
+            if modes[2]:
+                p[i + 3] = value
+            else:
+                p[int(p[i + 3])] = value
+            i += 4
 
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         program = f.read().split(',')
-    
-    run(program)
+
+    run(program, '5')
