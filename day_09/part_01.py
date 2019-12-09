086c534b6a9d3e1c56dbbe9afd0062058f1e72c9
jamsidedown <robertanderson1993@googlemail.com>
2019-12-09 12:06:21 +0000

    Refactored d9 to use defaultdict

--- day_09/part_01.py
+++ day_09/part_01.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 
@@ -5,7 +6,7 @@ class IntcodeComputer:
     II = {1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 9: 2}
 
     def __init__(self, program: List[str]):
-        self.program = {i: int(x) for i, x in enumerate(program)}
+        self.program = defaultdict(lambda: 0, {i: int(x) for i, x in enumerate(program)})
         self.i = 0
         self.rbase = 0
 
@@ -18,7 +19,7 @@ class IntcodeComputer:
         ii = self.II
 
         while (p[i] != 99):
-            instruction = str(p.get(i, 0)).rjust(5, '0')
+            instruction = str(p[i]).rjust(5, '0')
             opcode = int(instruction[-2:])
             modes = [instruction[2], instruction[1], instruction[0]]
             modes = [int(mode) for mode in modes]
@@ -27,27 +28,26 @@ class IntcodeComputer:
                 break
 
             if opcode in {1, 2, 4, 5, 6, 7, 8, 9}:
-                first = (p.get(p.get(i + 1, 0), 0) if modes[0] == 0
-                         else p.get(i + 1, 0) if modes[0] == 1
-                         else p.get(p.get(i + 1, 0) + self.rbase, 0))
+                first = (p[p[i + 1]] if modes[0] == 0
+                         else p[i + 1] if modes[0] == 1
+                         else p[p[i + 1] + self.rbase])
             if opcode in {1, 2, 5, 6, 7, 8}:
-                second = (p.get(p.get(i + 2, 0), 0) if modes[1] == 0
-                          else p.get(i + 2, 0) if modes[1] == 1
-                          else p.get(p.get(i + 2, 0) + self.rbase, 0))
+                second = (p[p[i + 2]] if modes[1] == 0
+                          else p[i + 2] if modes[1] == 1
+                          else p[p[i + 2] + self.rbase])
             if opcode in {1, 2, 7, 8}:
-                address = (p.get(i + 3, 0) if modes[2] == 0
+                address = (p[i + 3] if modes[2] == 0
                            else i + 3 if modes[2] == 1
-                           else p.get(i + 3, 0) + self.rbase)
-            if opcode in {3}:
-                address = (p.get(i + 1, 0) if modes[0] == 0
-                           else i + 1 if modes[0] == 1
-                           else p.get(i + 1, 0) + self.rbase)
+                           else p[i + 3] + self.rbase)
 
             if opcode == 1:
                 p[address] = first + second
             elif opcode == 2:
                 p[address] = first * second
             elif opcode == 3:
+                address = (p[i + 1] if modes[0] == 0
+                           else i + 1 if modes[0] == 1
+                           else p[i + 1] + self.rbase)
                 p[address] = input_parameter
             elif opcode == 4:
                 print(first)
